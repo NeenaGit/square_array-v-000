b52ee06042cf1ddb9fb47d0ad0660be0ff3dad13
Neena van de Water <nkuriakose@hotmail.com>
2017-11-06 15:33:26 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,9 +4,9 @@ def square_array(array)
   array.each{ | num | num ** 2 }
 end
 
-empty_array= []
-
-new_array = empty_array<< square_array 
+# empty_array= []
+# 
+# new_array = empty_array << square_array
 
 
 # squared_nums = []
