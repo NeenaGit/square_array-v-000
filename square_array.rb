ffe365541f0ba7854f9edb584b6c28af5af1bb80
Neena van de Water <nkuriakose@hotmail.com>
2017-11-06 15:52:59 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,11 @@
 
 
 def square_array(array)
-  empty_array= []
-  empty_array << array.each{ | num | num ** 2 }
-  return empty_array
+  sqr_nums= []
+  array.each do | num |
+    num ** 2
+  end
+  # empty_array << array.each{ | num | num ** 2 }
+  return sqr_nums << num ** 2
 end
 
-# empty_array= []
-#
-# new_array = empty_array << square_array
-
-
-# squared_nums = []
-
-# square_array(squared_nums)
